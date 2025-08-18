738918020ed0624b83d8669ff862d71cf52d5ca5
Vinicius Santos <viniciussantos@192.168.0.6>
2025-08-18 05:21:24 -0300

    Added more validations in reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -11,9 +11,42 @@ class Reservation < ApplicationRecord
 
   validates :entry_date_time,
             presence: true,
-            comparison: { less_than: :out_date_time }
+            comparison: { less_than: :out_date_time },
+            uniqueness: true
 
   validates :out_date_time,
             presence: true,
-            comparison: { greater_than: :entry_date_time }
+            comparison: { greater_than: :entry_date_time },
+            uniqueness: true
+
+  validate :block_friday_reservations
+
+  validate :block_weekend_reservations_with_out_date_in_same_day
+
+  validate :block_period_reservations
+
+  private
+
+  def block_friday_reservations
+    return unless (entry_date_time.to_date..out_date_time.to_date).any?(&:friday?)
+
+    errors.add(:entry_date_time, "Can't create a reservation on a friday")
+  end
+
+  def block_weekend_reservations_with_out_date_in_same_day
+    if entry_date_time.saturday? || entry_date_time.sunday? && out_date_time.to_date == entry_date_time.to_date
+      errors.add(:entry_date_time, "Can't create a reservation on weekend in the same day!")
+    end
+  end
+
+  def block_period_reservations
+    start_date = entry_date_time.beginning_of_month
+    end_date = entry_date_time.end_of_month
+    month_reservations = Reservation.where(entry_date_time: start_date..end_date)
+    month_reservations.each do |date_taken|
+      if (entry_date_time.to_date..out_date_time.to_date).overlaps?(date_taken.entry_date_time.to_date..date_taken.out_date_time.to_date)
+        errors.add(:entry_date_time, 'This period of time has already been taken!')
+      end
+    end
+  end
 end
